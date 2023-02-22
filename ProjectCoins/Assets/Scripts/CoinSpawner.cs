984b3ea7a169f682a4a9a14958ab43043d523ef6
Aleksandr Makarov <Mynterlid@gmail.com>
2023-02-22 22:26:19 +0300

    Create small spawn system
    Without random spawn.

--- ProjectCoins/Assets/Scripts/CoinSpawner.cs
+++ ProjectCoins/Assets/Scripts/CoinSpawner.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const int _minCountBigCoins = 40;
+    const int _minCountSmallCoins = 40;
+    
+    [SerializeField] private GameObject _bigCoin;
+    [SerializeField] private Transform _bigCoinTransform;
+    [SerializeField] private GameObject _smallCoin;
+    [SerializeField] private Transform _smallCoinTransform;
+    
+    [SerializeField][Range(80, 150)] private int _maxCountAllCoins;
+    
+    private int _maxCountBigCoins;
+    private int _maxCountSmallCoins;
+    
     void Start()
     {
+        _maxCountBigCoins = _maxCountAllCoins - _minCountBigCoins;
+        _maxCountSmallCoins = _maxCountAllCoins - _minCountSmallCoins ;
         
+        for (int _countCoins = 1; _countCoins <= _maxCountAllCoins; _countCoins++)
+        {
+            SpawnCoins(_countCoins);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnCoins(int _countCoins)
     {
-        
+        Debug.Log(_countCoins);
+        if (_countCoins <= _maxCountBigCoins)
+        {
+            Instantiate(_bigCoin, _bigCoinTransform);
+        }
+        else
+        {
+            Instantiate(_smallCoin, _smallCoinTransform);
+        }
     }
 }
