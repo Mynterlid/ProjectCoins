5d3be5633bdc1933bf87546ca3ed2588243e9d73
Aleksandr Makarov <Mynterlid@gmail.com>
2023-02-24 23:03:27 +0300

    Create Coins scripts

---
+++ ProjectCoins/Assets/Scripts/CoinS/SmallCoin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCoin : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    /*private void OnTriggerStay(Collider other)
+    {
+        throw new NotImplementedException();
+    }*/
+}
