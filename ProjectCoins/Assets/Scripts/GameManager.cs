a6180ab0872ca2e94bdb4f4e45ac2071d729cc5f
Aleksandr Makarov <Mynterlid@gmail.com>
2023-02-28 10:56:06 +0300

    Create interface Pause menu
    With adaptive UI

--- ProjectCoins/Assets/Scripts/GameManager.cs
+++ ProjectCoins/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,47 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    [SerializeField] private GameObject _ButtonE; 
+    [SerializeField] private GameObject _buttonE;
+    [SerializeField] private GameObject _pauseMenu;
     private void Start()
     {
         Instance = this;
     }
 
+    private void Update()
+    {
+        CheckInput();
+    }
+
     public void ActivateEButton()
     {
-        _ButtonE.SetActive(true);
+        _buttonE.SetActive(true);
     }
 
     public void DeactivateEButton()
     {
-        _ButtonE.SetActive(false);
+        _buttonE.SetActive(false);
     }
 
+    private void CheckInput()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            OpenPauseMenu();
+        }
+    }
+
+    private void OpenPauseMenu()
+    {
+        if (_pauseMenu.activeSelf)
+        {
+            _pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            _pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
 }
