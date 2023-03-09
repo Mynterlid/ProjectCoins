00627d08aef8a094dcc3912f276b5b10bdd2807e
Aleksandr Makarov <Mynterlid@gmail.com>
2023-03-09 21:58:58 +0300

    Small refactor PlayerMove and create Menu panel

--- ProjectCoins/Assets/Scripts/MenuSystem/MenuGameManager.cs
+++ ProjectCoins/Assets/Scripts/MenuSystem/MenuGameManager.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class PauseMenu : MonoBehaviour
+public class MenuGameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- ProjectCoins/Assets/Scripts/Player.cs
+++ ProjectCoins/Assets/Scripts/Player.cs
@@ -26,39 +26,51 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
-            int middleWidthScreen = Screen.width / 2;
-
-            if (Input.mousePosition.x <= middleWidthScreen)
-            {
-                transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition),  Time.deltaTime);
-                _playerRenderer.flipX = true;
-            }
-            else if (Input.mousePosition.x > middleWidthScreen)
-            {
-                transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition),  Time.deltaTime);
-                _playerRenderer.flipX = false;
-            }
+            MouseMove();
         }
-        else
+
+        KeyboardMove();
+    }
+
+    private void MouseMove()
+    {
+        int middleWidthScreen = Screen.width / 2;
+
+        if (Input.mousePosition.x <= middleWidthScreen)
         {
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.Translate(Vector2.up * (_speed * Time.deltaTime));
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Translate(Vector2.left * (_speed * Time.deltaTime));
-                _playerRenderer.flipX = true;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(Vector2.down * (_speed * Time.deltaTime));
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(Vector2.right * (_speed * Time.deltaTime));
-                _playerRenderer.flipX = false;
-            }
+            transform.position = Vector2.Lerp(transform.position, 
+                                            Camera.main.ScreenToWorldPoint(Input.mousePosition),  
+                                            Time.deltaTime);
+            _playerRenderer.flipX = true;
+        }
+        else if (Input.mousePosition.x > middleWidthScreen)
+        {
+            transform.position = Vector2.Lerp(transform.position, 
+                                            Camera.main.ScreenToWorldPoint(Input.mousePosition),  
+                                            Time.deltaTime);
+            _playerRenderer.flipX = false;
+        }
+    }
+
+    private void KeyboardMove()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * (_speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * (_speed * Time.deltaTime));
+            _playerRenderer.flipX = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * (_speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * (_speed * Time.deltaTime));
+            _playerRenderer.flipX = false;
         }
     }
 }
