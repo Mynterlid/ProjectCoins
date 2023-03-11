3f63d6fc5bdc6b2eec21dd7e52cc3a7da47e44ec
Aleksandr Makarov <Mynterlid@gmail.com>
2023-03-11 23:05:48 +0300

    Create Control Settings with prefs

--- ProjectCoins/Assets/Scripts/MenuSystem/Settings.cs
+++ ProjectCoins/Assets/Scripts/MenuSystem/Settings.cs
@@ -2,8 +2,71 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Settings : MonoBehaviour
 {
+    [SerializeField] private GameObject _keyboardOn;
+    [SerializeField] private GameObject _keyboardOff;
+    [SerializeField] private GameObject _mouseAndKeyboardOn;
+    [SerializeField] private GameObject _mouseAndKeyboardOff;
+    [SerializeField] private GameObject _mouseOn;
+    [SerializeField] private GameObject _mouseOff;
     
+    private void Start()
+    {
+        switch (PlayerPrefs.GetString("MoveSettings"))
+        {
+            case "Keyboard": KeyboardButton();
+                break;
+                
+            case "MouseAndKeyboard": MouseAndKeyboardButton();
+                break;
+            
+            case "Mouse": MouseButton();
+                break;
+        }
+    }
+
+    public void ToMenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void KeyboardButton()
+    {
+        DoButtonsOff();
+        PlayerPrefs.SetString("MoveSettings", "Keyboard");
+        _keyboardOff.SetActive(false);
+        _keyboardOn.SetActive(true);
+    }
+
+    public void MouseAndKeyboardButton()
+    {
+        DoButtonsOff();
+        PlayerPrefs.SetString("MoveSettings", "MouseAndKeyboard");
+        _mouseAndKeyboardOff.SetActive(false);
+        _mouseAndKeyboardOn.SetActive(true);
+        
+    }
+    
+    public void MouseButton()
+    {
+        DoButtonsOff();
+        PlayerPrefs.SetString("MoveSettings", "Mouse");
+        _mouseOff.SetActive(false);
+        _mouseOn.SetActive(true);
+    }
+
+    private void DoButtonsOff()
+    {
+        _keyboardOn.SetActive(false);
+        _mouseAndKeyboardOn.SetActive(false);
+        _mouseOn.SetActive(false);
+        
+        _keyboardOff.SetActive(true);
+        _mouseAndKeyboardOff.SetActive(true);
+        _mouseOff.SetActive(true);
+    }
 }
