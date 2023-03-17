210e5d72699f03f4656ce7ce8a517e26f45e33cc
Aleksandr Makarov <Mynterlid@gmail.com>
2023-03-17 20:27:02 +0300

    First update PlayerMove with control speed

--- ProjectCoins/Assets/Scripts/Player.cs
+++ ProjectCoins/Assets/Scripts/Player.cs
@@ -1,3 +1,6 @@
+using System;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -16,6 +19,11 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+        /*Debug.Log($"{Camera.main.ScreenToWorldPoint(Input.mousePosition)} - camera position");
+        Debug.Log(Vector2.Lerp(transform.position, 
+            Camera.main.ScreenToWorldPoint(Input.mousePosition),  
+            0));*/
+       
     }
 
     private void MovePlayer()
@@ -46,23 +54,60 @@ public class Player : MonoBehaviour
     private void MouseMove()
     {
         int middleWidthScreen = Screen.width / 2;
+        
+        Vector2 LerpPostion = Vector2.Lerp(transform.position,
+            MousePointPosition(),
+            Time.deltaTime);
 
         if (Input.mousePosition.x <= middleWidthScreen)
         {
-            transform.position = Vector2.Lerp(transform.position, 
-                                            Camera.main.ScreenToWorldPoint(Input.mousePosition),  
-                                            _speed / 100);
+            transform.position = LerpPostion;
             _playerRenderer.flipX = true;
         }
         else if (Input.mousePosition.x > middleWidthScreen)
         {
-            transform.position = Vector2.Lerp(transform.position, 
-                                            Camera.main.ScreenToWorldPoint(Input.mousePosition),  
-                                            _speed / 100);
+            transform.position = LerpPostion;
             _playerRenderer.flipX = false;
         }
     }
 
+    private Vector2 MousePointPosition()
+    {
+        Vector2 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (math.abs(MousePoint.x - transform.position.x) * Time.deltaTime > _speed * Time.deltaTime)
+        {
+            MousePoint.x = MousePoint.x - transform.position.x < 0 
+                ? transform.position.x - 10
+                : transform.position.x + 10;
+        }
+        
+        if (math.abs(MousePoint.y - transform.position.y) * Time.deltaTime 
+            > _speed * Time.deltaTime)
+        {
+            MousePoint.y = MousePoint.y - transform.position.y < 0 
+                ? transform.position.y - 10
+                : transform.position.y + 10;
+        }
+
+        float test = math.sqrt(math.pow(MousePoint.x - transform.position.x, 2) + math.pow(MousePoint.y - transform.position.y, 2));
+
+        /*if (math.abs(MousePoint.x - transform.position.x) * Time.deltaTime 
+            + math.abs(MousePoint.y - transform.position.y) * Time.deltaTime > _speed * Time.deltaTime)
+        {
+                        
+        }*/
+
+        MousePoint = new Vector2(transform.position.x + (MousePoint.x - transform.position.x) * _speed / test, transform.position.y + (MousePoint.y - transform.position.y) * );
+        
+        
+        //Debug.Log(math.sqrt(math.pow(MousePoint.x - transform.position.x, 2) + math.pow(MousePoint.y - transform.position.y, 2)));
+        
+        Debug.Log(_speed / test);
+        return MousePoint;
+    }
+    
+    
     private void KeyboardMove()
     {
         if (Input.GetKey(KeyCode.W))
